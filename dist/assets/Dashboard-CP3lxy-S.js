4cdc6eadbff2e1d21a58e50e04a4aa7f15533d03
vinicius45-cmd <vinicius.45710@gmail.com>
2026-07-25 18:36:47 -0300

    Tirei a caixa ao lado da opção Ver todos

--- dist/assets/Dashboard-CP3lxy-S.js
+++ dist/assets/Dashboard-CP3lxy-S.js
@@ -1,4 +1,4 @@
-import{c as x,r as l,j as e,E as D,a as V,M as T,C as w,F as H}from"./index-B0pkcqS7.js";/**
+import{c as x,r as l,j as e,E as D,a as V,M as T,C as w,F as H}from"./index-5sk-yscj.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
